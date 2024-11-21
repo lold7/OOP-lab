b091f28d3c2f93f58ba626010d26dd649e1c318d
earth <earthearth13218@gamil.com>
2024-11-21 16:07:30 +0700

    add

---
+++ review.java
@@ -0,0 +1,12 @@
+public class review{
+    public static void main(String[] args) {
+        int j = 0;
+        int result =1;
+        for(int i = 0 , k = 1 ; i<30 ;i++,k++ ){
+            result = (int) Math.pow(2,i);
+            j = j+result;
+            System.out.println("day " + k + " deposit "+ result + " total is " +j);
+        }
+    }
+}
+    
