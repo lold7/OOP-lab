4fc13d8c66768d2c5f09f787f0bff36b390e4acd
earth <earthearth13218@gamil.com>
2024-11-28 10:59:33 +0700

    fjrfj

--- Dog.java
+++ Dog.java
@@ -2,9 +2,7 @@ public class Dog {
     private String color;
     private float weight;
 
-    //public void setcolor(String x ){color = x;}
-    //public void setweight(float y){weight = y;}
-
+    
     public Dog(String x , float y){
         color = x;
         weight = y;
