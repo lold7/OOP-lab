8856c85523abe08ca3aea11bf814f4dabf7cfabe
earth <earthearth13218@gamil.com>
2024-12-05 13:58:11 +0700

    update

---
+++ OOP/Counter.java
@@ -0,0 +1,22 @@
+package OOP;
+public class Counter {
+
+    private int count;
+
+    public void zero(){
+        this.count = 0;
+    }
+
+    public void click(){
+        count++;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void reset(){
+        count = 0;
+    }
+
+}
